770dbc4116e39f615848107fba29920025ba5b86
szollosyi <145103349+szollosyi@users.noreply.github.com>
2025-02-10 20:23:15 +0100

    Új user

--- bee_healthy.sql
+++ bee_healthy.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 15, 2024 at 03:05 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Feb 10. 20:22
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,35 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bee_healthy`
+-- Adatbázis: `bee_healthy`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `felhasznaloi_adatok`
---
-
-CREATE TABLE `felhasznaloi_adatok` (
-  `Id` int(11) NOT NULL,
-  `Nev` varchar(64) NOT NULL,
-  `Email` varchar(64) NOT NULL,
-  `Jelszo` varchar(20) NOT NULL,
-  `Szuletesi_datum` varchar(11) NOT NULL,
-  `Kapcsolat` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
-
---
--- Dumping data for table `felhasznaloi_adatok`
---
-
-INSERT INTO `felhasznaloi_adatok` (`Id`, `Nev`, `Email`, `Jelszo`, `Szuletesi_datum`, `Kapcsolat`) VALUES
-(1, 'Tihanyi  Tihamér', 'ttihamer@gmail.com', 'tihanyiak', '1990.09.09', 'Tihany\r\nTelefonszám : +36301234567');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `gyogyszer_adatok`
+-- Tábla szerkezet ehhez a táblához `gyogyszer_adatok`
 --
 
 CREATE TABLE `gyogyszer_adatok` (
@@ -62,43 +40,71 @@ CREATE TABLE `gyogyszer_adatok` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- Dumping data for table `gyogyszer_adatok`
+-- A tábla adatainak kiíratása `gyogyszer_adatok`
 --
 
 INSERT INTO `gyogyszer_adatok` (`Id`, `Gyogyszer_nev`, `Marka`, `Kategoria`, `Adagolas`, `Kezelesi_idopont`, `Kezeles_idotartama`, `Emlekezteto`, `Megjegyzes`) VALUES
 (1, 'Algopyrin', 'SANOFI', 'Láz - és fájdalomcsillapító', 'Napi max 2 ', 'Délben és este 7 órakor', 'Egy héten át', '2024-12-15', 'Ha a fájdalom nem múlik , az adagolás száma növelhető max 1 darabszámal');
 
+-- --------------------------------------------------------
+
 --
--- Indexes for dumped tables
+-- Tábla szerkezet ehhez a táblához `user`
+--
+
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL,
+  `LoginNev` varchar(16) NOT NULL,
+  `HASH` varchar(64) NOT NULL,
+  `SALT` varchar(64) NOT NULL,
+  `Name` varchar(64) NOT NULL,
+  `PermissionId` int(11) NOT NULL,
+  `Active` tinyint(1) NOT NULL,
+  `Email` varchar(64) NOT NULL,
+  `ProfilePicturePath` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `user`
+--
+
+INSERT INTO `user` (`Id`, `LoginNev`, `HASH`, `SALT`, `Name`, `PermissionId`, `Active`, `Email`, `ProfilePicturePath`) VALUES
+(1, 'kerenyir', 'd5fe0e517520122f1ab363b6b7ee9ae616e7ad393693ef00d81a7f287a79931a', 'Gm63C4jiWnYvfZfiKUu2cu8AHPNDj8NoHhtQn88yiJhyOunBNSd7tRoWo5wwqg9X', 'Kerényi Róbert', 9, 1, 'kerenyir@kkszki.hu', 'img\\kerenyir.jpg');
+
+--
+-- Indexek a kiírt táblákhoz
 --
 
 --
--- Indexes for table `felhasznaloi_adatok`
---
-ALTER TABLE `felhasznaloi_adatok`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indexes for table `gyogyszer_adatok`
+-- A tábla indexei `gyogyszer_adatok`
 --
 ALTER TABLE `gyogyszer_adatok`
   ADD PRIMARY KEY (`Id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- A tábla indexei `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `LoginNev` (`LoginNev`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `Jog` (`PermissionId`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
 --
 
 --
--- AUTO_INCREMENT for table `felhasznaloi_adatok`
---
-ALTER TABLE `felhasznaloi_adatok`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `gyogyszer_adatok`
+-- AUTO_INCREMENT a táblához `gyogyszer_adatok`
 --
 ALTER TABLE `gyogyszer_adatok`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT a táblához `user`
+--
+ALTER TABLE `user`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
