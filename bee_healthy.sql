0502f35ee85d5ff286955e83ab6ad72fafd574b7
TiszaBalazs <147139280+TiszaBalazs@users.noreply.github.com>
2025-02-11 07:35:51 +0100

    Gyarto adatok hozzáadása

--- bee_healthy.sql
+++ bee_healthy.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Feb 10. 20:22
+-- Létrehozás ideje: 2025. Feb 11. 07:33
 -- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.0.30
+-- PHP verzió: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tábla szerkezet ehhez a táblához `gyarto`
+--
+
+CREATE TABLE `gyarto` (
+  `Id` int(11) NOT NULL,
+  `Nev` varchar(64) NOT NULL,
+  `Cim` varchar(64) NOT NULL,
+  `Leiras` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `gyarto`
+--
+
+INSERT INTO `gyarto` (`Id`, `Nev`, `Cim`, `Leiras`) VALUES
+(1, 'Pfhizer', 'USA', 'Szuri');
+
+-- --------------------------------------------------------
+
+--
 -- Tábla szerkezet ehhez a táblához `gyogyszer_adatok`
 --
 
@@ -31,6 +51,7 @@ CREATE TABLE `gyogyszer_adatok` (
   `Id` int(11) NOT NULL,
   `Gyogyszer_nev` varchar(64) NOT NULL,
   `Marka` varchar(64) NOT NULL,
+  `GyartoId` int(11) NOT NULL,
   `Kategoria` varchar(64) NOT NULL,
   `Adagolas` varchar(64) NOT NULL,
   `Kezelesi_idopont` varchar(64) NOT NULL,
@@ -43,8 +64,9 @@ CREATE TABLE `gyogyszer_adatok` (
 -- A tábla adatainak kiíratása `gyogyszer_adatok`
 --
 
-INSERT INTO `gyogyszer_adatok` (`Id`, `Gyogyszer_nev`, `Marka`, `Kategoria`, `Adagolas`, `Kezelesi_idopont`, `Kezeles_idotartama`, `Emlekezteto`, `Megjegyzes`) VALUES
-(1, 'Algopyrin', 'SANOFI', 'Láz - és fájdalomcsillapító', 'Napi max 2 ', 'Délben és este 7 órakor', 'Egy héten át', '2024-12-15', 'Ha a fájdalom nem múlik , az adagolás száma növelhető max 1 darabszámal');
+INSERT INTO `gyogyszer_adatok` (`Id`, `Gyogyszer_nev`, `Marka`, `GyartoId`, `Kategoria`, `Adagolas`, `Kezelesi_idopont`, `Kezeles_idotartama`, `Emlekezteto`, `Megjegyzes`) VALUES
+(1, 'Algopyrin', 'SANOFI', 1, 'Láz - és fájdalomcsillapító', 'Napi max 2 ', 'Délben és este 7 órakor', 'Egy héten át', '2024-12-15', 'Ha a fájdalom nem múlik , az adagolás száma növelhető max 1 darabszámal'),
+(3, 'string', 'string', 1, 'string', 'string', 'string', 'string', '2025-02-10', 'string');
 
 -- --------------------------------------------------------
 
@@ -69,17 +91,27 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`Id`, `LoginNev`, `HASH`, `SALT`, `Name`, `PermissionId`, `Active`, `Email`, `ProfilePicturePath`) VALUES
-(1, 'kerenyir', 'd5fe0e517520122f1ab363b6b7ee9ae616e7ad393693ef00d81a7f287a79931a', 'Gm63C4jiWnYvfZfiKUu2cu8AHPNDj8NoHhtQn88yiJhyOunBNSd7tRoWo5wwqg9X', 'Kerényi Róbert', 9, 1, 'kerenyir@kkszki.hu', 'img\\kerenyir.jpg');
+(1, 'kerenyir', 'd5fe0e517520122f1ab363b6b7ee9ae616e7ad393693ef00d81a7f287a79931a', 'Gm63C4jiWnYvfZfiKUu2cu8AHPNDj8NoHhtQn88yiJhyOunBNSd7tRoWo5wwqg9X', 'Kerényi Róbert', 9, 1, 'kerenyir@kkszki.hu', 'default.jpg'),
+(11, 'string', '473287f8298dba7163a897908958f7c0eae733e25d2e027992ea2edc9bed2fa8', 'string', 'string', 1, 0, 'zimaz@kkszki.hu', 'string'),
+(14, 'string3', '2984285', '9526984', 'string', 0, 1, 'string3', 'string'),
+(15, 'UjEmber', '1042b9e964b97f96099bcafd747e56ca04169f5e81a5374dab227362e95832f5', 'kY4Lma0h1ZNM5iz1SvD1chpkOECkHG8inGYcPHQYi51vnNu02ctEPZeUfz2QkKyU', 'Új ember', 9, 1, 'ftvzgbuhnijmko', 'Nincs kiválasztva');
 
 --
 -- Indexek a kiírt táblákhoz
 --
 
 --
+-- A tábla indexei `gyarto`
+--
+ALTER TABLE `gyarto`
+  ADD PRIMARY KEY (`Id`);
+
+--
 -- A tábla indexei `gyogyszer_adatok`
 --
 ALTER TABLE `gyogyszer_adatok`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `GyartoId` (`GyartoId`);
 
 --
 -- A tábla indexei `user`
@@ -95,16 +127,32 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT a táblához `gyarto`
+--
+ALTER TABLE `gyarto`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT a táblához `gyogyszer_adatok`
 --
 ALTER TABLE `gyogyszer_adatok`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT a táblához `user`
 --
 ALTER TABLE `user`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `gyogyszer_adatok`
+--
+ALTER TABLE `gyogyszer_adatok`
+  ADD CONSTRAINT `gyogyszer_adatok_ibfk_1` FOREIGN KEY (`GyartoId`) REFERENCES `gyarto` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
