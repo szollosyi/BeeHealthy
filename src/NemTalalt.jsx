1fe02571891eb0c58b289f38ca9b1bd125c98ddb
szollosyi <145103349+szollosyi@users.noreply.github.com>
2025-02-12 08:20:10 +0100

    Add files via upload

---
+++ src/NemTalalt.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const NemTalalt = () => (
+  <div>
+    <h1>404 - Az oldal nem található!</h1>
+    <p>Próbálj meg egy másik menüpontot választani.</p>
+  </div>
+);
