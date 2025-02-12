48d44f448062993668d96e6e18f7e8ec4e2cf1e5
szollosyi <145103349+szollosyi@users.noreply.github.com>
2025-02-12 08:19:36 +0100

    Delete beehealthy directory

--- beehealthy/src/Kezdolap.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Kezdőlap = () => (
-  <div>
-    <h1>Üdvözöllek a Gyógyszerkezelő Appban!</h1>
-    <p>Válassz a fenti menüből.</p>
-  </div>
-);
--- beehealthy/src/NemTalalt.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const NemTalalt = () => (
-  <div>
-    <h1>404 - Az oldal nem található!</h1>
-    <p>Próbálj meg egy másik menüpontot választani.</p>
-  </div>
-);
